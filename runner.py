9564170e9eb74f1aa8ca055c651c3b56fdfb5975
ineedsleepfs <114431199+ineedsleepfs@users.noreply.github.com>
2022-12-06 16:57:22 +0000

    Create runner.py

---
+++ runner.py
@@ -0,0 +1,8 @@
+from data_collector import DataCollector
+
+if __name__ == "__main__":
+    data_collector = DataCollector()
+    data_collector.getOrgSize()
+    data_collector.getIndustries()
+    data_collector.getCountries()
+    data_collector.getRevenue()
